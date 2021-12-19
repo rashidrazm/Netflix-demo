4336c385a0dc7fe6f4621be272de9c9e791b6c99
mohammed rashid <razmsi28@gmail.com>
2021-12-19 18:58:27 +0530

    website completely change into dynamic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from './Components/NavBar/NavBar';
+import { originals,action } from './urls'
 import './App.css'
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
@@ -8,7 +9,9 @@ function App() {
     <div className="app">
         <NavBar/>
         <Banner/>
-        <RowPost/>
+        <RowPost url={originals}   title='Netflix Originals' />
+        <RowPost url={action} title='Action Movies' isSmall /> 
+        
     </div>
   );
 }
